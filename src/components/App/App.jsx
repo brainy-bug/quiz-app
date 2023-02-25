bcc721d7fb76b0a4bb38fa06d42e71e5f23e4be4
maodesigns <mao.devv@gmail.com>
2023-02-25 07:39:28 +0100

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+import { useGlobalContext } from "../../context";
+import { Modal, Loading, SetupForm } from "../../components";
+
+function App() {
+  return <h2>quiz starter</h2>;
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import App from "./App/App";
+import Loading from "./Loading/Loading";
+import Modal from "./Modal/Modal";
+import SetupForm from "./SetupForm/SetupForm";
+
+export { App, Loading, Modal, SetupForm };
